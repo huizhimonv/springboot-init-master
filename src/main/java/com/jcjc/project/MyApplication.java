a889661da4e04503ea84fb542807df4429b4ed73
huizhimonv <2647695814@qq.com>
2022-09-25 20:33:02 +0800

    用spring cache缓存套餐数据

--- src/main/java/com/jcjc/project/MyApplication.java
+++ src/main/java/com/jcjc/project/MyApplication.java
@@ -5,12 +5,14 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Slf4j
 @SpringBootApplication
 @MapperScan("com.jcjc.project.mapper")
 @ServletComponentScan
+@EnableCaching  //开启spring cache注解的缓存功能
 @EnableTransactionManagement
 public class MyApplication {
 
--- src/main/java/com/jcjc/project/controller/SetmealController.java
+++ src/main/java/com/jcjc/project/controller/SetmealController.java
@@ -13,7 +13,10 @@ import com.jcjc.project.service.SetmealService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
+
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -39,6 +42,7 @@ public class SetmealController {
      * @return
      */
     @PostMapping
+    @CacheEvict(value = "setmealCache",allEntries = true)
     public R<String> save(@RequestBody SetmealDto setmealDto){
         setmealService.saveWithDish(setmealDto);
         return R.success("新增套餐成功");
@@ -93,12 +97,14 @@ public class SetmealController {
      * @return
      */
     @DeleteMapping
+    @CacheEvict(value = "setmealCache",allEntries = true)
     public R<String> delete(@RequestParam List<Long> ids){
         setmealService.removeWithDish(ids);
         return R.success("删除成功");
     }
 
     @GetMapping("/list")
+    @Cacheable(value = "setmealCache",key ="#setmeal.categoryId + '_' + #setmeal.status")
     public R<List<Setmeal>> list(Setmeal setmeal){
         //条件构造器
         LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
